c84c8f1e493c3e1a8f0fd406b0ec14a5ead4a076
wechtits19 <martin.wechtitsch@edu.fh-joanneum.at>
2025-01-08 20:19:23 +0100

    actually use api url from env

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 import Entry from './Entry';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 
 const fetchAvailabilities = async (): Promise<Entry[]> => {
     const response = await axios.get(`${API_URL}/availability`);
