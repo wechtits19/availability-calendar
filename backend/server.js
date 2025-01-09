4454176aa0d0c081c90f086711e5ced40c650382
wechtits19 <martin.wechtitsch@edu.fh-joanneum.at>
2025-01-09 11:44:03 +0100

    include env for build

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '../dist')));
 
 // Connect to MongoDB
-const connectionString = process.env.MONGODB_PRIMARY_CONNECTION_STRING || 'mongodb://user:pass@localhost:27017?directConnection=true';
+const connectionString = process.env.REACT_APP_MONGODB_PRIMARY_CONNECTION_STRING || 'mongodb://user:pass@localhost:27017?directConnection=true';
 
 mongoose.connect(connectionString)
     .then(() => {
